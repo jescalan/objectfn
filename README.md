659133ede2302fcc653f2181f4b7cd418b2c51a4
Declan de Wet <declandewet@users.noreply.github.com>
2016-06-15 01:02:00 +0200

    Readabilty + Documentation Tweaks && Add Index (#1)
    * fix incorrect attribution link in readme
    * improve readability
    * improve documentation
    * add support for index
    
    * remove use strict directive

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Map, reduce, forEach, and filter for objects. Lazy evaluation, no dependencies.
 
 I wanted a library that has no dependencies and gives me the basic map/reduce/filter for use on objects. Any existing library I found has boatloads of dependencies, provides tons more extra tools, and/or is unmaintained. So here's `ObjectFn`, just for you!
 
-Also, big props to [@declandewet](https://github.com/jescalan) for the initial implementation of this library!
+Also, big props to [@declandewet](https://github.com/declandewet) for the initial implementation of this library!
 
 ### Installation
 
@@ -24,14 +24,36 @@ Very straightforward usage. Just import what you need and use it on an object.
 const {map, reduce, filter, forEach} = require('objectfn')
 
 const obj = { foo: 'bar', wow: 'doge' }
-map(obj, (v, k) => v.toUpperCase())
+
+map(obj, (val, key) => val.toUpperCase())
 // { foo: 'BAR', wow: 'DOGE' }
-reduce(obj, (m, v, k) => m[v.toUpperCase()] = k && m }, {})
+
+reduce(obj, (accum, val, key) => accum[val.toUpperCase()] = key && accum }, {})
 // { FOO: 'bar', WOW: 'doge' }
-filter(obj, (v, k) => !k === 'foo')
+
+filter(obj, (val, key) => !key === 'foo')
 // { wow: 'doge' }
-forEach(obj, console.log)
+
+forEach(obj, console.log.bind(console))
 // logs out all the values
+```
+
+Each callback has a method signature of `(value, key, index, object)` with the exception of `reduce`, which has `(accumulator, value, key, index, object)`. `value` is the current key's value, `key` is the current key's name, `index` is the 0-based index of the current key and `object` is the original object.
+
+**Note:** Unlike the native array equivalent as well as other library implementations, we felt it would be better to explicitly require the passing of an accumulator to the `reduce` method.
+
+This means that this will work:
+
+```js
+let obj = { one: 1, two: 2, three: 3, four: 4 }
+reduce(obj, (accum, val) => accum + val, 0) // => 10
+```
+
+But this will not:
+
+```js
+let obj = { one: 1, two: 2, three: 3, four: 4 }
+reduce(obj, (prevVal, currVal) => prevVal + currVal) // => wat?
 ```
 
 ### License & Contributing
--- index.js
+++ index.js
@@ -1,38 +1,69 @@
-function * entries (obj) {
-  for (let key of Object.keys(obj)) {
-    yield [obj[key], key]
-  }
-}
-
-function map (obj, cb) {
+/**
+ * Iterates over an object's keys, applying a transform function to each value.
+ * @param  {Object}   obj       - the initial object
+ * @param  {Function} transform - the transform function. Receives `(value, key, index, object)` as arguments.
+ * @return {Object}             - the new object
+ */
+exports.map = function map (obj, transform) {
   let res = {}
+  let index = 0
   for (let [val, key] of entries(obj)) {
-    res[key] = cb(val, key, obj)
+    res[key] = transform(val, key, index++, obj)
   }
   return res
 }
 
-function filter (obj, cb) {
+/**
+ * Iterates over an object's keys, applying a predicate function that determines whether the current key will appear in the returned object.
+ * @param  {Object}   obj       - the initial object
+ * @param  {Function} predicate - the predicate function. Receives `(value, key, index, object)` as arguments. Should return `true` or `false`.
+ * @return {Object}             - the new object
+ */
+exports.filter = function filter (obj, predicate) {
   let res = {}
+  let index = 0
   for (let [val, key] of entries(obj)) {
-    if (cb(val, key, obj)) {
+    if (predicate(val, key, index++, obj)) {
       res[key] = val
     }
   }
   return res
 }
 
-function forEach (obj, cb) {
+/**
+ * Iterates over an object's keys, calling an iterator function on each pass.
+ * @param  {Object}   obj     - the initial object
+ * @param  {Function} iterate - the iterator function. Recives `(value, key, index, object)` as arguments.
+ */
+exports.forEach = function forEach (obj, iterate) {
+  let index = 0
   for (let [val, key] of entries(obj)) {
-    cb(val, key, obj)
+    iterate(val, key, index++, obj)
   }
 }
 
-function reduce (obj, cb, init) {
+/**
+ * Iterates over an object's keys, applying a reducer function on each pass that reduces the current parameters into a single value.
+ * @param  {Object}   obj         - the initial object.
+ * @param  {Function} reducer     - the reducer function. Receives `(accumulator, value, key, index, object)` as arguments.
+ * @param  {*}        accumulator - a value to accumulate results into.
+ * @return {*}                    - the combined accumulated value.
+ */
+exports.reduce = function reduce (obj, reducer, accumulator) {
+  let index = 0
   for (let [val, key] of entries(obj)) {
-    init = cb(init, val, key, obj)
+    accumulator = reducer(accumulator, val, key, index++, obj)
   }
-  return init
+  return accumulator
 }
 
-module.exports = {map, reduce, filter, forEach}
+/**
+ * Lazily iterates over an object's keys, yielding each current value and key as a [value, key] array.
+ * @param  {Object}                   obj - the initial object
+ * @return {iterable.<Array<String>>}     - an iterable of arrays containing object's [value, key]
+ */
+function * entries (obj) {
+  for (let key of Object.keys(obj)) {
+    yield [obj[key], key]
+  }
+}
